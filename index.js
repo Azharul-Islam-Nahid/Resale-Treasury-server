a95385e23bf90dc9eda69af7408ec0fa04cc5f36
Azharul Islam <lonewolfazharul@gmail.com>
2022-11-26 07:19:11 +0600

    jwt verify

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const app = express();
@@ -15,13 +16,30 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pepm1no.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access')
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         const phoneCategories = client.db('resaleTreasury').collection('categories');
         const categoryItems = client.db('resaleTreasury').collection('categoryItems');
         const usersCollection = client.db('resaleTreasury').collection('users');
 
-        app.get('/categories', async (req, res) => {
+        app.get('/categories', verifyJWT, async (req, res) => {
             const query = {}
             const category = await phoneCategories.find(query).toArray();
             res.send(category)
@@ -35,6 +53,27 @@ async function run() {
             const phones = await cursor.toArray()
             res.send(phones)
         })
+
+
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token });
+
+            }
+            res.status(403).send({ accessToken: 'Oops!' })
+        })
+
+
+        // const decodedEmail = req.decoded.email;
+
+        // if(email!==decodedEmail){
+        //     return res.status(403).send({message:'forbidden access'})
+        // }
 
 
         app.post('/users', async (req, res) => {
